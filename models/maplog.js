ec6c0a854bdbd65e7c281730836c5e22194cfae8
Aaron Moore <x2kicker2x@gmail.com>
2017-10-04 14:59:31 -0400

    Deploying

--- models/maplog.js
+++ models/maplog.js
@@ -1,6 +1,9 @@
 let mongoose = require('mongoose');
+//
+const Schema = mongoose.Schema;
 
-let leaderboardSchema = mongoose.Schema({
+// let leaderboardSchema = mongoose.Schema({
+let leaderboardSchema = new Schema({
 	username: String,
 	level: Number
 })
